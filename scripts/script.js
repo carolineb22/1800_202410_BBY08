556d9ad869aa8dc1541ceb99455d7e1faf6c23b0
Caroline <carolinebastiaa@gmail.com>
2024-12-02 15:22:01 -0800

    when user signs up for the first time it will save the date into their user collection

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 function sayHello() {
     
 }
-//sayHello();
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+document.write(today);
